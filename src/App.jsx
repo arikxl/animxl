ec1ae4313e1e6468b3ac3d28f4be863d09a37a43
arikxl <arikxl@gmail.com>
2025-08-17 22:20:53 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+
+      <h1  className='text-5xl text-violet-300 font-bold'>Welcome to Gamer</h1>
+    </main>
+  )
+}
+
+export default App
